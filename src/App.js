edefb15f3fa9e644934ec13e1e3606788664296c
Vioswift <kylewww@gmail.com>
2019-10-03 07:23:34 +1000

    Working on open file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function buildFileSelector(){
+    const fileSelector = document.createElement('input');
+    fileSelector.setAttribute('type', 'file');
+    fileSelector.setAttribute('multiple', 'multiple');
+    return fileSelector;
+  }
+  
   return (
+    componentDidMount() {
+      this.fileSelector = buildFileSelector();
+    }
+    
+    handleFileSelect = (e) => {
+      e.preventDefault();
+      this.fileSelector.click();
+    }
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TimeInLieuTracker</h1>
+        <a href="" onClick={this.handleFileSelect}>Select files</a>
       </header>
     </div>
   );
