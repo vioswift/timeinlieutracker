e33e94fca19099f3c3cfcd4254ad72146c59c464
vioswift <Kylewww@gmail.com>
2019-10-25 10:06:00 +1100

    Put json file data into main app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './App.css';
 import FileDialogue from './FileDialogue';
+import TimePanels from './TimePanels';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TimeInLieuTracker</h1>
-        
-        <FileDialogue/>
-        {/* <p>File Version: {FileDialogue.state.jsonFile.settings.show_signatures}</p> */}
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    jsonFile: []
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>TimeInLieuTracker</h1>
+          
+          <FileDialogue                         
+            fileData={data => {
+              this.setState({ jsonFile: data });
+            }}
+          />
+
+          <TimePanels json={this.state.jsonFile}/>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/FileDialogue.js
+++ src/FileDialogue.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TimePanels from './TimePanels';
 
 function buildFileSelector(){
     const fileSelector = document.createElement('input');
@@ -9,14 +8,6 @@ function buildFileSelector(){
 }
 
 class FileDialogue extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            jsonFile: []
-        };
-    }
-    
     componentDidMount(){
         this.fileSelector = buildFileSelector();
     }
@@ -28,21 +19,14 @@ class FileDialogue extends React.Component {
         e.preventDefault();
 
         reader.onload = () => {
-            this.setState({ jsonFile: JSON.parse(reader.result) }, () => {
-                // console.log(this.state.jsonFile.days);
-            });            
+            this.props.fileData(JSON.parse(reader.result));           
         };
 
         reader.readAsText(file);
     }
 
     render(){
-        return (
-            <div>
-                <input type="file" name="file" onChange={this.handleFileSelect.bind(this)}/>
-                <TimePanels json={this.state.jsonFile}/>
-            </div>
-        );
+        return <input type="file" name="file" onChange={this.handleFileSelect.bind(this)}/>
     }
 }
 
