d8998fb745c50ac3d892dc4abbb687391d2d3115
vioswift <Kylewww@gmail.com>
2019-10-28 16:57:32 +1100

    Deleted old Electron App, started new one, moved React App into the Electron App

--- public/electron.js
+++ public/electron.js
@@ -1,29 +1,40 @@
+
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, BrowserWindow } = electron;
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
-
-let mainWindow;
-
-function createWindow() {
-    mainWindow = new BrowserWindow({width: 900, height: 680});
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-    mainWindow.on('closed', () => mainWindow = null);
-}
+let mainWindow = null;
 
 app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
+app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
 });
 
-app.on('activate', () => {
+app.on('activate', function () {
     if (mainWindow === null) {
-        createWindow();
+        createWindow()
     }
 });
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 1024,
+        title: "Chat desktop app demo"
+    });
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+    mainWindow.on('page-title-updated', function (e) {
+        e.preventDefault()
+    });
+
+    if (isDev) {
+        // Open the DevTools.
+        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+        mainWindow.webContents.openDevTools();
+    }
+}
